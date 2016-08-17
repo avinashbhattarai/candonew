180be03946f8822e9213837f504a0dae1f6b6391
szubyak <SvyatZubyakMacBook@macbook-pro-svyat.local>
2016-08-17 13:23:17 +0300

    Initial Commit

---
+++ CanDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CanDo
+//
+//  Created by Svyat Zubyak MacBook on 17.08.16.
+//  Copyright © 2016 Svyat Zubyak MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
